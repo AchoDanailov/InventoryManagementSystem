1b23813dad170ce7fb8dd9feb0cd3fcb096dd7c9
A. Danailov <danailov.angel.99@gmail.com>
2026-01-16 00:52:44 +0200

    Add entity validation values to Location type's properties.

--- InventoryManagementSystem.Data.Models/Location.cs
+++ InventoryManagementSystem.Data.Models/Location.cs
@@ -1,20 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+using static InventoryManagementSystem.Common.EntityValidationConstants.Location;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace InventoryManagementSystem.Data.Models;
 
 /* Since the data stored in this type is optional, not relevant to the business logic
 and is always queried together with its owner's data, in the current version of the application,
-there is no need for a separate table.*/
+there is no need for a separate table and spatial data.*/
 [Owned]
 [Comment("Establishment's location")]
 public class Location
 {
+    [MaxLength(CountryMaxLengthValue)]
     [Comment("Establishment's location country name.")]
     public string? Country { get; set; }
     
+    [MaxLength(CityMaxLengthValue)]
     [Comment("Establishment's location city name.")]
     public string? City { get; set; }
     
+    [MaxLength(AddressMaxLengthValue)]
     [Comment("Establishment's location address.")]
     public string? Address { get; set; }
 }
